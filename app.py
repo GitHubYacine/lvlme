cb00ff4375832969ab97b9021e3ce06440e09d5b
Yacine <122400729+GitHubYacine@users.noreply.github.com>
2023-08-29 14:50:28 +0200

    2023089goodsession!!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -36,10 +36,19 @@ def update_skill_down():
     skills = Skill.query.all()
     return render_template("front.html", skills=skills)
 
+@app.route('/complete_challenge/<int:challenge_id>', methods=['POST'])
+def complete_challenge(challenge_id):
+    completed = request.form.get('completed') == 'true'
+    challenge = Challenge.query.get(challenge_id)
+    if challenge and completed:
+        update_experience(challenge_id)
+    return redirect(url_for('first_page'))
+
 class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     level = db.Column(db.Integer, nullable=False)
+    experience = db.Column(db.Float, default=0)
     description = db.Column(db.String(500), nullable=True)
     challenges = db.relationship('Challenge', backref='skill', lazy=True)
         
@@ -49,7 +58,18 @@ class Challenge(db.Model):
     description = db.Column(db.String(1000), nullable=False)
     experience = db.Column(db.Float(100), nullable=False)
     skill_id = db.Column(db.Integer, db.ForeignKey("skill.id"), nullable=False)
-
+    
+def update_experience(challenge_id):
+    challenge = Challenge.query.get(challenge_id)
+    experience_points = challenge.experience
+    corresponding_skill = challenge.skill
+    corresponding_skill.experience += int(experience_points)
+    if corresponding_skill.experience >= 100:
+        corresponding_skill.level += 1
+        corresponding_skill.experience -= 100
+    
+    db.session.commit()
+    
 if __name__ == '__main__':
     with app.app_context(): db.create_all()
     app.run(debug=True)
--- filldb.py
+++ filldb.py
@@ -1,18 +1,18 @@
 from app import app, Skill, db, Challenge
 
 sample_skills = [
-        {"name": "Strength", "level": 1, "description": "Strength represents your physical power."},
-        {"name": "Defence", "level": 1, "description": "Defence represents your ability to protect yourself either by fighting or discussing."},
-        {"name": "Prayer", "level": 1, "description": "Prayer represents your practice of communicating with a higher power seeking guidance, blessings, or inner peace."},
-        {"name": "Construction", "level": 1, "description": "Construction represents the art and science of building structures and infrastructures."},
-        {"name": "Heart", "level": 1, "description": "Heart represents your cardiovascular endurance and the efficiency of your circulatory system during physical activities."},
-        {"name": "Agility", "level": 1, "description": "Agility represents your ability to move quickly and easily, showcasing physical dexterity or adaptability."},
-        {"name": "Farming", "level": 1, "description": "Farming represents the cultivation of plants and rearing of animals to produce food and other products."},
-        {"name": "Gaming", "level": 1, "description": "Gaming represents the act of playing games, ranging from video games to board games."},
-        {"name": "Knowledge", "level": 1, "description": "Knowledge represents the accumulation of facts, information, and skills acquired through experience or education."},
-        {"name": "Cooking", "level": 1, "description": "Cooking represents the art and technique of preparing food by combining and heating ingredients."},
-        {"name": "Creativity", "level": 1, "description": "Creativity represents the ability to produce original and imaginative ideas or art."},
-        {"name": "Coding", "level": 1, "description": "Coding represents the practice of writing computer programs using programming languages."},
+        {"name": "Strength", "level": 1, "experience": 0, "description": "Strength represents your physical power."},
+        {"name": "Defence", "level": 1, "experience": 0, "description": "Defence represents your ability to protect yourself either by fighting or discussing."},
+        {"name": "Prayer", "level": 1, "experience": 0, "description": "Prayer represents your practice of communicating with a higher power seeking guidance, blessings, or inner peace."},
+        {"name": "Construction", "level": 1, "experience": 0, "description": "Construction represents the art and science of building structures and infrastructures."},
+        {"name": "Heart", "level": 1, "experience": 0, "description": "Heart represents your cardiovascular endurance and the efficiency of your circulatory system during physical activities."},
+        {"name": "Agility", "level": 1, "experience": 0, "description": "Agility represents your ability to move quickly and easily, showcasing physical dexterity or adaptability."},
+        {"name": "Farming", "level": 1, "experience": 0, "description": "Farming represents the cultivation of plants and rearing of animals to produce food and other products."},
+        {"name": "Gaming", "level": 1, "experience": 0, "description": "Gaming represents the act of playing games, ranging from video games to board games."},
+        {"name": "Knowledge", "level": 1, "experience": 0, "description": "Knowledge represents the accumulation of facts, information, and skills acquired through experience or education."},
+        {"name": "Cooking", "level": 1, "experience": 0, "description": "Cooking represents the art and technique of preparing food by combining and heating ingredients."},
+        {"name": "Creativity", "level": 1, "experience": 0, "description": "Creativity represents the ability to produce original and imaginative ideas or art."},
+        {"name": "Coding", "level": 1, "experience": 0, "description": "Coding represents the practice of writing computer programs using programming languages."},
     ]
 
 challenges_for_skills = [
@@ -23,9 +23,8 @@ challenges_for_skills = [
 ]
 
 with app.app_context():
-    if True == False:
-        for skill_data in sample_skills:
-            skills = Skill(name=skill_data["name"], level=skill_data["level"], description=skill_data["description"])
+    for skill_data in sample_skills:
+            skills = Skill(name=skill_data["name"], level=skill_data["level"], experience=skill_data["experience"], description=skill_data["description"])
             db.session.add(skills)
     for challenge in challenges_for_skills:
         challenges = Challenge(name=challenge["name"], description=challenge["description"], experience=challenge["experience"], skill_id=challenge["skill_id"])
